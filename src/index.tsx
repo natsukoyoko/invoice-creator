4920b83501765cc6524b3d563e515248dbf54d52
natsukoyoko <genspark_dev@genspark.ai>
2026-02-20 03:43:10 +0000

    Move JCT Registration Number explanatory note to Name field for better layout

--- src/index.tsx
+++ src/index.tsx
@@ -198,6 +198,9 @@ app.get('/', (c) => {
                             </label>
                             <input type="text" name="issuerName" required
                                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-transparent">
+                            <p class="text-xs text-gray-500 mt-2">
+                                <strong>JCT Registration Number:</strong> If you are a registered tax entity in Japan, please provide your JCT Registration Number (starting with 'T'). If you are a non-resident without a registration number, please leave this field blank.
+                            </p>
                         </div>
                         
                         <div>
@@ -207,9 +210,6 @@ app.get('/', (c) => {
                             <input type="text" name="issuerTNumber"
                                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                                    placeholder="T1234567890123">
-                            <p class="text-xs text-gray-500 mt-1">
-                                If you are a registered tax entity in Japan, please provide your JCT Registration Number (starting with 'T'). If you are a non-resident without a registration number, please leave this field blank.
-                            </p>
                         </div>
                         
                         <div class="md:col-span-2">
